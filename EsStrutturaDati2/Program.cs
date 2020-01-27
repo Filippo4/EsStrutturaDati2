cef54596704f2eac58ba74078612803d47759bd8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:38:14 +0100

    Add project files.

---
+++ EsStrutturaDati2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsStrutturaDati2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> numeri = new HashSet<int>();
+            using (StreamReader sr = new StreamReader("fileInteri.txt",Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    int n = int.Parse(line);
+                    numeri.Add(n);
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("fileSalvati.txt", false, Encoding.UTF8))
+            {
+                foreach(int s in numeri)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+        }
+    }
+}
